89dba680d90547a1c112f1e93e006be20bc1d6f3
Admin <admin@Admins-Mac-mini.local>
2018-01-07 20:31:34 -0500

    added ajax fetch

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -5,56 +5,56 @@ class Chart extends Component{
 
     constructor(props){
         super(props);
+
         this.state = {
-            chartData:{
-                labels:[
-                    '2017-01-01',
-                    '2017-01-02',
-                    '2017-01-03',
-                    '2017-01-04',
-                    '2017-01-05'
-                ],
-                datasets:[
-                    {
-                        label:'BTC',
-                        data:[
-                            1100,
-                            1500,
-                            1800,
-                            2100,
-                            2800
-                        ],
-                        borderWidth: 1
-                    },
-                    {
-                        label:'BCH',
-                        data:[
-                            100,
-                            110,
-                            125,
-                            160,
-                            180
-                        ],
-                        borderWidth: 1
-                    },
-                    {
-                        label:'ETH',
-                        data:[
-                            1000,
-                            1005,
-                            1018,
-                            1165,
-                            1250
-                        ],
-                        borderWidth: 1
-                    }
-                ]
-            }
+            isLoading: true
         }
     }
 
+    componentDidMount() {
+        return fetch('https://tarragon-server.gadgetlabs.com/assets?startDate=2017-01-01&endDate=2017-01-07&granularity=day')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log(responseJson);
+                this.setState({
+                    isLoading: false,
+                    chartData:{
+                        labels:['2017-01-01', '2017-01-02', '2017-01-03', '2017-01-04', '2017-01-05'],
+                        datasets:[
+                            {
+                                label:'BTC',
+                                data:[1100, 1500, 1800, 2100, 2800],
+                                borderWidth: 1
+                            },
+                            {
+                                label:'BCH',
+                                data:[100, 110, 125, 160, 180],
+                                borderWidth: 1
+                            },
+                            {
+                                label:'ETH',
+                                data:[1000, 1005, 1018, 1165, 1250],
+                                borderWidth: 1
+                            }
+                        ]
+                    }
+                }, function() {
+                    // do something with new state
+                });
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
     render() {
+        if (this.state.isLoading) {
+            return (
+                <div className="loading">LOADING</div>
+            )
+        }
         return (
+
             <div className="chart" >
                 <Line
                     data={this.state.chartData}
